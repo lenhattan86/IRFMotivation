57570b4726e4ff01eb34483b27cc546cb80d98a6
Tan N. Le <lenhattan86@gmail.com>
2017-09-04 10:20:30 -0400

    update

--- tf_analysis/benchmarks/linear_regression.py
+++ tf_analysis/benchmarks/linear_regression.py
@@ -20,10 +20,10 @@ display_step = 50
 #train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167, 7.042,10.791,5.313,7.997,5.654,9.27,3.1])                         
 #train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221, 2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 #n_samples = train_X.shape[0]
-n_samples=1000
 
-train_X=rng.rand(n_samples,1)
-train_Y=rng.rand(n_samples,1)
+n_samples=10000
+train_X=rng.rand(1,n_samples)
+train_Y=rng.rand(1,n_samples)
 
 
 with tf.device('/gpu:0'):
