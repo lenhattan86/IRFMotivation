64d2311836c5eab6527a5f4ed4b38f427c639fc3
Tan N. Le <lenhattan86@gmail.com>
2017-09-04 09:51:12 -0400

    update

--- tf_analysis/benchmarks/linear_regression.py
+++ tf_analysis/benchmarks/linear_regression.py
@@ -13,15 +13,17 @@ rng = numpy.random
 
 # Parameters
 learning_rate = 0.01
-training_epochs = 50000
+training_epochs = 5000
 display_step = 50
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
-n_samples = train_X.shape[0]
+#train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167, 7.042,10.791,5.313,7.997,5.654,9.27,3.1])                         
+#train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221, 2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+#n_samples = train_X.shape[0]
+n_samples=1000
+
+train_X=rng.rand(n_samples,1)
+train_Y=rng.rand(n_samples,1)
 
 
 with tf.device('/gpu:0'):
@@ -46,11 +48,11 @@ with tf.device('/gpu:0'):
     init = tf.global_variables_initializer()
 
     # gpu share
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2)
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2)
 
 # Launch the graph
-with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-#with tf.Session() as sess:
+#with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+with tf.Session() as sess:
     sess.run(init)
 
     # Fit all training data
