249aa38b859a0444092442d6229593557936c48e
Tan N. Le <lenhattan86@gmail.com>
2017-10-04 21:41:52 -0400

    quick update

--- localclientsetup.sh
+++ localclientsetup.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #usage: ./localclientsetup.sh [username] [masterIPaddress] [keyfilelocation]
+echo "./localclientsetup.sh [username] [masterIPaddress] [keyfilelocation]"
 if [ -z "$1" ]
 then
-	hostname="Chameleon/EC-2"
+	username="cc"
 else
-	hostname="$1"
+	username="$1"
 fi
 if [ -z "$2" ]
 then
-	hostIP=129.114.109.80
+	hostIP="129.114.108.146"
 else
 	hostIP="$2"
 fi
 if [ -z "$3" ]
 then
-	keyfile=~/chamcloud.key
+	keyfile="~/Dropbox/Research/chameleoncloud/chameleon.pem"
 else
 	keyfile="$3"
 fi
@@ -28,7 +29,13 @@ apt-get update"
 sudo apt-get install -y kubectl
 #brew install kubectl
 #mkdir -p  ~/.kube/admin.conf
-scp -i $keyfile $hostname@$hostIP:~/config/admin.conf ~/.kube/admin.conf
+#echo "ssh -i $keyfile $username@$hostIP \"echo hello $hostIP\""
+ssh -i $keyfile $username@$hostIP "echo hello $hostIP"
+scp -i $keyfile $username@$hostIP:~/config/admin.conf ~/.kube/admin.conf
 export KUBECONFIG=~/.kube/admin.conf
 kubectl create -f https://git.io/kube-dashboard
 echo "######################### Dashboard is now installed, you can run \$kubectl proxy to view the Dashboard ##########################################"
+#sudo systemctl daemon-reload
+#sudo systemctl restart kubelet
+# if it does not work, try export KUBECONFIG=~/.kube/admin.conf; Check $kubectl version
+
